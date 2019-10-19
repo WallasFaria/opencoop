92b6527245345e6d632559569592f44df1dbc5a8
Wallas Faria <wallasfaria@hotmail.com>
2019-10-19 06:49:44 -0300

    adiciona mascara nos campos de data cep e telefone

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("jquery-mask-plugin")
+const masks = require("./masks")(window.jQuery)
 
+document.addEventListener('turbolinks:load', function() {
+  masks.apply()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
