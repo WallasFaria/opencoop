0105bc4be1d14b0c750f4e051392cbc5e1cc8f6e
Wallas Faria <wallasfaria@hotmail.com>
2019-10-19 14:23:42 -0300

    trata url do vídeo antes salva

--- app/models/cooperative.rb
+++ app/models/cooperative.rb
@@ -5,6 +5,7 @@ class Cooperative < ApplicationRecord
   belongs_to :founder, class_name: 'Associate'
   has_many :accounts, as: :owner
 
+  before_create :handler_video_url
   after_create :set_default_status, :create_account
 
   private
@@ -15,5 +16,11 @@ class Cooperative < ApplicationRecord
 
   def set_default_status
     self.status = :project
+  end
+
+  def handler_video_url
+    utr_template = 'https://www.youtube.com/embed/:id?autoplay=1'
+    match_id = video_url.match(/v=([a-zA-Z0-9_]+)/) if video_url
+    self.video_url = utr_template.sub(':id', match_id[1]) if match_id
   end
 end
