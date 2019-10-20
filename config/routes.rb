40089b6407f5143153d833a2e43fffc24a0aaead
Jaqueline Passos do Nascimento <jaqueline.passos@popsolutions.co>
2019-10-19 21:53:38 -0300

    Criação da página de transações e ajustes no css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'transactions/index'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :cooperatives do
     resources :cooperations, only: [:new, :create]
